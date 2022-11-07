4d56e2b470bed455bf20b740651a0abd36e800a0
NirnayK <nirnaykorde@gmail.com>
2022-11-07 19:28:19 +0530

    Repair Tabs Component

--- frontend/src/Pages/Faq.js
+++ frontend/src/Pages/Faq.js
@@ -1,9 +1,10 @@
 import React from "react";
+import BasicTabs from "../Components/Nirnay/Tabs";
 
 function Faq() {
   return (
     <div>
-      <h1>Got any Questions? Welcome to FAQ Page</h1>
+      <BasicTabs/>
     </div>
   );
 }
