239360c674f7306a646c0e39c641d80b557a4708
NirnayK <nirnaykorde@gmail.com>
2022-11-07 14:27:19 +0530

    Add Expandable Card Component in the About Page

--- frontend/src/Pages/About.js
+++ frontend/src/Pages/About.js
@@ -1,9 +1,10 @@
 import React from "react";
+import ExpandableCard from "../Components/Nirnay/ExpandableCard";
 
 function About() {
   return (
     <div>
-      <h1>This is the About Page</h1>
+      <ExpandableCard />
     </div>
   );
 }
