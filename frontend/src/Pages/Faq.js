b52a7ee0b630a765d62194f3137fa5551860001f
Raghav Khanna <raghav.29.khanna@gmail.com>
2022-11-07 19:32:48 +0530

    Add MUI Dependencies for Tab Component

--- frontend/src/Pages/Faq.js
+++ frontend/src/Pages/Faq.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Tabs from "../Components/Nirnay/Tabs";
 
 function Faq() {
   return (
     <div>
       <h1>Got any Questions? Welcome to FAQ Page</h1>
+      <Tabs />
     </div>
   );
 }
