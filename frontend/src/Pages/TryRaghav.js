4ab088ead0930d2fdb8a98e02aa50fe5a81a8d37
Raghav Khanna <raghav.29.khanna@gmail.com>
2022-11-10 20:59:09 +0530

    Co-authored-by: NirnayK <NirnayK@users.noreply.github.com>

--- frontend/src/Pages/TryRaghav.js
+++ frontend/src/Pages/TryRaghav.js
@@ -1,9 +1,11 @@
 import React from "react";
+import DisplayCard from "../Components/Raghav/DisplayCard";
 
 const TryRaghav = () => {
   return (
     <div>
       <h1>Welcome to Try-Raghav Page</h1>
+      <DisplayCard />
     </div>
   );
 };
