172f65d1b4ed83e787340a58ad66db6922b8676e
NirnayK <nirnaykorde@gmail.com>
2022-11-13 01:07:02 +0530

    Sections Improve

--- frontend/src/Components/Nirnay/SideTable.js
+++ frontend/src/Components/Nirnay/SideTable.js
@@ -2,24 +2,24 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./SideTable.css";
 
 const SideTable = () => {
   return (
     <ListGroup className="listgrp" defaultActiveKey="#link1">
-      <ListGroup.Item action href="#link1">
+      <ListGroup.Item className="Buton" action href="#link1">
         Link 1
       </ListGroup.Item>
-      <ListGroup.Item action href="#link2">
+      <ListGroup.Item className="Buton" action href="#link2">
         Link 2
       </ListGroup.Item>
-      <ListGroup.Item action href="#link3">
+      <ListGroup.Item className="Buton" action href="#link3">
         Link 3
       </ListGroup.Item>
-      <ListGroup.Item action href="#link4">
+      <ListGroup.Item className="Buton" action href="#link4">
         Link 4
       </ListGroup.Item>
     </ListGroup>
+
   );
 };
 
