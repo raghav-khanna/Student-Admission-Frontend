8b44d02afbe660946c5a3984bcd8e254cf905e12
NirnayK <nirnaykorde@gmail.com>
2022-11-13 19:11:29 +0530

    "AboutUS Feature"

--- frontend/src/Pages/TryNirnay.js
+++ frontend/src/Pages/TryNirnay.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ExpandContentButton from "../Components/Nirnay/ExpandContentButton";
 
 
 const TryNirnay = () => {
   return (
     <div>
-
+      <ExpandContentButton />
     </div>
   );
 };
