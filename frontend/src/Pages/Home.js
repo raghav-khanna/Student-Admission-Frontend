d81ce920a46b4c9869afdf2b400f705bc4c33713
NirnayK <nirnaykorde@gmail.com>
2022-11-17 00:34:04 +0530

    Add The go to top component in the home page.

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -4,6 +4,7 @@ import BannerCards from "../Components/Prateek/BannerCards";
 import InstituteGlance from "../Components/Prateek/InstituteGlance";
 import PhotoGallery from "../Components/Nirnay/PhotoGallery";
 import Footer from "../Components/Prateek/Footer";
+import GoToTop from "../Components/Nirnay/GoToTop";
 
 function Home() {
   return (
@@ -13,6 +14,7 @@ function Home() {
       <PhotoGallery />
       <InstituteGlance />
       <Footer />
+      <GoToTop />
     </div>
   );
 }
