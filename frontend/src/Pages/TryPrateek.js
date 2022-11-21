ef5fa24ec5c09a3d088eba4d1a594a71f895e00e
NirnayK <nirnaykorde@gmail.com>
2022-11-22 01:28:06 +0530

    Nothing

--- frontend/src/Pages/TryPrateek.js
+++ frontend/src/Pages/TryPrateek.js
@@ -1,11 +1,8 @@
 import React from "react";
-// import UG_Highlight_section from "../Components/Nirnay/UG_Highlight_section";
-import UG_Highlight_section from "../Components/Prateek/UG_Highlight_section";
 
 const TryPrateek = () => {
   return (
     <div>
-      <UG_Highlight_section />
     </div>
   );
 };
