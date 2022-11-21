1e520c7c9e134e5fec33c9318885045c79bf26c7
Raghav Khanna <raghav.29.khanna@gmail.com>
2022-11-21 23:01:46 +0530

    Add 2 more branches in preferences

--- frontend/src/Pages/Login - First Time/ChoosePreferences.js
+++ frontend/src/Pages/Login - First Time/ChoosePreferences.js
@@ -23,8 +23,8 @@ const ChoosePreferences = () => {
       <Form className="details-form" onSubmit={handleSubmit}>
         <h1>Enter Your Academic Details</h1>
         <br />
-        <Row className="mb-5">
-          <Form.Group as={Col}>
+        <Row className="mb-3">
+          <Form.Group className="mb-5">
             <Form.Label>Preference - 1</Form.Label>
             <Form.Select size="lg" defaultValue="Choose...">
               <option>Choose...</option>
@@ -32,9 +32,13 @@ const ChoosePreferences = () => {
               <option>Computer and Communication Engineering (CCE) </option>
               <option>Electrical and Communication Engineering (ECE) </option>
               <option>Mechanical and Mechatronics Engineering (MME) </option>
+              <option>Dual Degree Computer Science Engineering (DCS) </option>
+              <option>
+                Dual Degree Electrical and Communication Engineering (DEC)
+              </option>
             </Form.Select>
           </Form.Group>
-          <Form.Group as={Col}>
+          <Form.Group className="mb-5">
             <Form.Label>Preference - 2</Form.Label>
             <Form.Select size="lg" defaultValue="Choose...">
               <option>Choose...</option>
@@ -42,11 +46,13 @@ const ChoosePreferences = () => {
               <option>Computer and Communication Engineering (CCE) </option>
               <option>Electrical and Communication Engineering (ECE) </option>
               <option>Mechanical and Mechatronics Engineering (MME) </option>
+              <option>Dual Degree Computer Science Engineering (DCS) </option>
+              <option>
+                Dual Degree Electrical and Communication Engineering (DEC)
+              </option>
             </Form.Select>
           </Form.Group>
-        </Row>
-        <Row className="mb-5">
-          <Form.Group as={Col}>
+          <Form.Group className="mb-5">
             <Form.Label>Preference - 3</Form.Label>
             <Form.Select size="lg" defaultValue="Choose...">
               <option>Choose...</option>
@@ -54,9 +60,13 @@ const ChoosePreferences = () => {
               <option>Computer and Communication Engineering (CCE) </option>
               <option>Electrical and Communication Engineering (ECE) </option>
               <option>Mechanical and Mechatronics Engineering (MME) </option>
+              <option>Dual Degree Computer Science Engineering (DCS) </option>
+              <option>
+                Dual Degree Electrical and Communication Engineering (DEC)
+              </option>
             </Form.Select>
           </Form.Group>
-          <Form.Group as={Col}>
+          <Form.Group className="mb-5">
             <Form.Label>Preference - 4</Form.Label>
             <Form.Select size="lg" defaultValue="Choose...">
               <option>Choose...</option>
@@ -64,6 +74,38 @@ const ChoosePreferences = () => {
               <option>Computer and Communication Engineering (CCE) </option>
               <option>Electrical and Communication Engineering (ECE) </option>
               <option>Mechanical and Mechatronics Engineering (MME) </option>
+              <option>Dual Degree Computer Science Engineering (DCS) </option>
+              <option>
+                Dual Degree Electrical and Communication Engineering (DEC)
+              </option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-5">
+            <Form.Label>Preference - 5</Form.Label>
+            <Form.Select size="lg" defaultValue="Choose...">
+              <option>Choose...</option>
+              <option>Computer Science Engineering (CSE) </option>
+              <option>Computer and Communication Engineering (CCE) </option>
+              <option>Electrical and Communication Engineering (ECE) </option>
+              <option>Mechanical and Mechatronics Engineering (MME) </option>
+              <option>Dual Degree Computer Science Engineering (DCS) </option>
+              <option>
+                Dual Degree Electrical and Communication Engineering (DEC)
+              </option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-5">
+            <Form.Label>Preference - 6</Form.Label>
+            <Form.Select size="lg" defaultValue="Choose...">
+              <option>Choose...</option>
+              <option>Computer Science Engineering (CSE) </option>
+              <option>Computer and Communication Engineering (CCE) </option>
+              <option>Electrical and Communication Engineering (ECE) </option>
+              <option>Mechanical and Mechatronics Engineering (MME) </option>
+              <option>Dual Degree Computer Science Engineering (DCS) </option>
+              <option>
+                Dual Degree Electrical and Communication Engineering (DEC)
+              </option>
             </Form.Select>
           </Form.Group>
         </Row>
