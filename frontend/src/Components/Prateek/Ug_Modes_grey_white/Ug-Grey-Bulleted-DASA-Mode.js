bcac9ea44e937fc4f9de06a655a1932e49a1ccfa
prateek4321 <106796411+prateek4321@users.noreply.github.com>
2022-11-22 14:46:58 +0530

    UG admissions grey and white table part (#29)

---
+++ frontend/src/Components/Prateek/Ug_Modes_grey_white/Ug-Grey-Bulleted-DASA-Mode.js
@@ -0,0 +1,19 @@
+const arr1head_admission_category=["Admission Category"];
+const arr1data_admission_category=[["Admission under this mode is open for applicants under following three categories:
+    ["Foreign Nationals (FN)/ Overseas Citizens of India (OCI)
+        ["Candidate must have a valid passport issued by a foreign country"]",
+    "Non-resident Indian (NRI)
+        ["Candidate must have completed her/his Class 11th and 12th or equivalent from outside India."]",
+    "Children of Indian Workers in Gulf Countries (CIWG)
+        ["Candidate must have completed her/his Class 11th and 12th or equivalent from a gulf country (Bahrain, Iraq, Iran, Kuwait, Oman, Qatar, Saudi Arabia, UAE) and atleast one of the parent must be working in a gulf country"]"]"]];
+
+const arr2head_preparation_of_merit_list=["Preparation of Merit List"];
+const arr2data_preparation_of_merit_list=[["Separate Merit Lists will be prepared on the basis of: (i) valid SAT score (ii) Final NTA score (percentile) obtained in Paper-1 (B.E./B.Tech.) in JEE (Main) 2022 Examination, for all eligible applicants.",
+"In case of identical Final NTA Score (Percentile), the following tie-breaker rules will be applied.
+    ["Mathematics, Physics, and Chemistry total percentage in class 12th examination",
+    "Mathematics marks in class 12th examination",
+    "Physics marks in class 12th examination",
+    "Chemistry marks in class 12th examination",
+    "Total percentage of marks in Class 10th examination"]",
+    "Seats will be allotted as per the Merit Lists."]];
+
