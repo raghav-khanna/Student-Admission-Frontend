5b79c630f09d6c7b2304a48c59c5ef6fea857339
NirnayK <75352087+NirnayK@users.noreply.github.com>
2022-11-22 14:37:12 +0530

    UG Front Page  (#27)
    * Minor changes fixed footer
    
    * Resolve issue of previous commit
    
    * Changes made to file names
    
    * Nothing
    
    * Rename Files and resolve conficts
    
    * Stuff
    
    * UG Base Page Complete
    
    * Intermediate Commit for UgAdmissions
    
    * Intermediate Commit 2
    
    * Minor changes

---
+++ frontend/src/Components/UG/UgHead.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./UgHead.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+const UgHead = () => {
+    return (
+        <section className="ug-mode">
+            <div className="ug-mode-container">
+                <p className="ug-mode-p">
+                    LNMIIT offers <strong>B.Tech</strong> in Communication and Computer Engineering (CCE), Computer Science and Engineering (CSE), Electronics and Communication Engineering (ECE), and Mechanical Engineering (ME) and <strong>B. Tech.-M. Tech. (5-Year Integrated Dual Degree)</strong> in Computer Science and Engineering (CSE) and Electronics and Communication Engineering (ECE). Admissions to the above mentioned programmes are through following modes.
+                </p>
+                <p className="ug-mode-p-red">
+                    <strong>Important Note:</strong> All admissions to LNMIIT Jaipur are handled ONLY through the admission portal on our website. We do NOT have any other companies/ individuals or online portals representing us and providing admission services.
+                </p>
+                <Button
+                    size="lg"
+                    variant="danger"
+                    className="btn"
+                >
+                    APPLY NOW
+                </Button>
+            </div>
+        </section>
+    )
+}
+
+export default UgHead
