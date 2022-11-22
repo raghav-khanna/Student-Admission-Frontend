0b1cc9e81f080be76d571ad47f6aa1bbf1da96c7
prateek4321 <20ucs145@lnmiit.ac.in>
2022-11-22 15:49:29 +0530

    grey-white components of all modes

---
+++ frontend/src/Components/Prateek/Pg-Engineering-Mode-grey-white/Pg-Engineering-White-Bulleted.js
@@ -0,0 +1,23 @@
+const arr1head_imp_note_under_programmes=["Important Note"];
+const arr1data_imp_note_under_programmes=[["All admissions to LNMIIT Jaipur are handled ONLY through the admission portal on our website. We do NOT have any other companies/ individuals or online portals representing us and providing admission services. "]];
+
+const arr2head_under_imp_dates=["Written exam details:","Note:"];
+const arr2data_under_imp_dates=[["Mode: Online","Type of ques.: MCQ","Syllabus: GATE","Duration: 1 hour (time will be notified by email) "],
+["The Institute reserves the right to reflect any changes with respect to the admission to a programme, number of rounds of seat allocations, respective deadlines, and also any changes as may be necessary in view of statutory requirements/ notifications from time-to-time."]];
+
+const arr3head_selection_procedure=["Selection Procedure"];
+const arr3data_selection_procedure=[["GATE: Based on valid GATE score in concerned subject a merit list will be prepared solely based on GATE score for the applicants who satisfy the eligibility criteria and have a valid GATE score in concerned subject and have secured marks above the qualifying marks in the General Category",
+"Non-GATE: Based on performance in the institute conducted written test, ll Non-Gate applicants who satisfy the eligibility criteria have to appear for an institute conducted written test and a separate merit list will be prepared based on the applicant's performance in the written test.",
+"Admissions will be made based on the above prepared merit lists.",
+"Note: Incase seats remain vacant under one of the mode (GATE/Non-GATE), then those seats will be transferred to the other mode."]];
+
+const arr4head_note_under_hostel_mess_charges=["NOTE:"];
+const arr4data_note_under_hostel_mess_charges=[["Zero effective Tuition Fee under HTTA/HTRA scheme. For details please check the financial support section.",
+"Group Medical-cum-Accidental Insurance Charges shall be levied yearly on actual basis from 2nd Semester onwards.",
+"Lifetime membership for LNMIIT Alumni Association of INR 2,000/- (one time) will be charged from all graduating students.",
+"The Institute reviews the Fee Structure & charges periodically. There will be an annual increase of approximately 10% in Fee/charges.",
+"Electricity and Internet charges may vary depending upon the rates/charges levied by respective board/service provider.",
+"Summer course fee/charges and bus facility charges are payable extra, if opted.",
+"Staying in the Hostel is optional. Hostel facility would be available subject to availability of the rooms.",
+"Hostel and Mess charges would apply only to those provided with hostel accommodation and using the mess."]];
+
---
+++ frontend/src/Components/Prateek/Pg-Science-Mode-grey-white/Pg-Science-White-Bulleted.js
@@ -0,0 +1,2 @@
+const arr1head_imp_note=["Important Note"];
+const arr1data_imp_note=[["All admissions to LNMIIT Jaipur are handled ONLY through the admission portal on our website. We do NOT have any other companies/ individuals or online portals representing us and providing admission services"]];
