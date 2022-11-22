37c9d0e6e4e6dee1a0c5f9843633286e8465c5da
NirnayK <nirnaykorde@gmail.com>
2022-11-22 21:14:38 +0530

    Scholarship Component Created

--- frontend/src/Pages/UgRegularMode.js
+++ frontend/src/Pages/UgRegularMode.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import UgModes from '../Components/UG/UgModes';
 import UgModeFirst from '../Components/UG/UgModeFirst';
-import UgImpDates from '../Components/UG/UgImpDates';
 
 const UgRegularMode = () => {
     return (
