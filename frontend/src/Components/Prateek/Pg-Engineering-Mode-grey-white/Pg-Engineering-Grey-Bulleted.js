2d64fed2beff276046aa984539c267fec8715a14
prateek4321 <20ucs145@lnmiit.ac.in>
2022-11-22 20:00:05 +0530

    all modes grey-white part updated

--- frontend/src/Components/Prateek/Pg-Engineering-Mode-grey-white/Pg-Engineering-Grey-Bulleted.js
+++ frontend/src/Components/Prateek/Pg-Engineering-Mode-grey-white/Pg-Engineering-Grey-Bulleted.js
@@ -1,13 +1,16 @@
-const arr1head_note_under_eligibility_criteria=["Note:"];
-const arr1data_note_under_eligibility_criteria=[["1. Candidates appearing for final examinations in 2022 can also apply."]];
+const arr1head_note_under_eligibility_criteria = ["Note:"];
+const arr1data_note_under_eligibility_criteria = [
+  "1. Candidates appearing for final examinations in 2022 can also apply.",
+];
 
-const arr2head_instructions_to_apply=["Instructions to Apply"];
-const arr2data_instructions_to_apply=[["To apply for admission in M.Tech./M.S. programme, interested applicants have to necessarily fill the online application form through the admission portal.Application Form other than online mode would not be accepted in any case. Only one application needs to be submitted by an applicant. More than one application, i.e. multiple applications submitted by an applicant will be rejected. The applicant is required to pay an application fee (non-refundable) of INR 500. For, Women applicant, application fee is INR 250."]];
+const arr2head_instructions_to_apply = ["Instructions to Apply"];
+const arr2data_instructions_to_apply = [
+  "To apply for admission in M.Tech./M.S. programme, interested applicants have to necessarily fill the online application form through the admission portal.Application Form other than online mode would not be accepted in any case. Only one application needs to be submitted by an applicant. More than one application, i.e. multiple applications submitted by an applicant will be rejected. The applicant is required to pay an application fee (non-refundable) of INR 500. For, Women applicant, application fee is INR 250.",
+];
 
-const arr3head_financial_support=["Financial Support"];
-const arr3data_financial_support=[["M. Tech. students with valid GATE score shall receive scholarship from AICTE as per rules.",
-"Financial support is available as a half-time teaching / research assistant (HTTA / HTRA) as per the rules of the institute. The HTTAship / HTRAship under this scheme will be equivalent to the full Tuition Fee, and thus making it zero effective tuition fee. This will be extended to the next three semesters, provided the candidate is able to maintain a minimum of 7 CGPA and do satisfactory work as assessed by the Department.",
-"Limited financial assistance for national and international travel to present research work in reputed (Tier - I / Tier - II) referred National / International conferences."]];
-
-const arrhead=[" "," "];
-const arrdata=[[" "],[" "]];
+const arr3head_financial_support = ["Financial Support"];
+const arr3data_financial_support = [
+  "M. Tech. students with valid GATE score shall receive scholarship from AICTE as per rules.",
+  "Financial support is available as a half-time teaching / research assistant (HTTA / HTRA) as per the rules of the institute. The HTTAship / HTRAship under this scheme will be equivalent to the full Tuition Fee, and thus making it zero effective tuition fee. This will be extended to the next three semesters, provided the candidate is able to maintain a minimum of 7 CGPA and do satisfactory work as assessed by the Department.",
+  "Limited financial assistance for national and international travel to present research work in reputed (Tier - I / Tier - II) referred National / International conferences.",
+];
