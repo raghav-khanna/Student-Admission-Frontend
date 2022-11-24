e3ef72705228bef2b23e60a54ee99900b62a4495
Raghav Khanna <raghav.29.khanna@gmail.com>
2022-11-24 09:27:47 +0530

    Add link to Allotment Page

--- frontend/src/Pages/After Login/ApplicantHome.js
+++ frontend/src/Pages/After Login/ApplicantHome.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 const ApplicantHome = () => {
   return (
     <div>
       <h1>Welcome to the Home Page of the Applicant!</h1>
+      <Link to={"/applicant/allotment"}>
+        <Button className="m-3" size={"lg"} variant={"danger"}>
+          Go to Allotment Page
+        </Button>
+      </Link>
     </div>
   );
 };
